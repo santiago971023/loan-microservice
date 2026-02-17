c8a9b3918181d12dd9adf1d4edd65d3b154ff91a
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-17 11:07:27 -0500

    Se procede con la creación del microservicio y con la estructura de carpetas.

---
+++ src/test/java/com/loan_microservice/LoanMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
