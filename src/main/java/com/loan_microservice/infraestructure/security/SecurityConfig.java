7e8cd40eb190d4d3c71159a783481f06796fbd69
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-25 12:03:54 -0500

    feat (creación de solicitudes): Se crea clase SecurityConfig, clase que fija las rutas protegidas por role. Y que además en el filtro, llama a nuestro Manager y nuestro Converter.

--- src/main/java/com/loan_microservice/infraestructure/security/SecurityConfig.java
+++ src/main/java/com/loan_microservice/infraestructure/security/SecurityConfig.java
@@ -1,4 +1,47 @@
 package com.loan_microservice.infraestructure.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationConverter jwtAuthenticationConverter;
+    private final JwtAuthenticationManager jwtAuthenticationManager;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+
+        AuthenticationWebFilter jwtFilter = new AuthenticationWebFilter(jwtAuthenticationManager);
+        jwtFilter.setServerAuthenticationConverter(jwtAuthenticationConverter);
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+
+                .authorizeExchange(exchanges -> exchanges
+                        // Rutas públicas
+
+
+                        // Rutas protegidas por role
+                        .pathMatchers("/api/v1/loans").hasAnyRole("CUSTOMER")
+
+                        // Rutas no especificadas.
+                        .anyExchange().authenticated()
+                        )
+                .addFilterAt(jwtFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .build();
+
+    }
+
+
 }
