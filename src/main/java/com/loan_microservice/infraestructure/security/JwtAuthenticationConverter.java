a5df7f1b9526f57ad3e346d3cad2b7c26b455d2e
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-25 11:57:31 -0500

    feat (creación de solicitudes): Se crea clase JwtAuthenticationConverter.java, se encarga de validar el token, además de extraerle el prefijo "Bearer ", funcion necesaria en nuestro SecurityConfig.

--- src/main/java/com/loan_microservice/infraestructure/security/JwtAuthenticationConverter.java
+++ src/main/java/com/loan_microservice/infraestructure/security/JwtAuthenticationConverter.java
@@ -1,4 +1,34 @@
 package com.loan_microservice.infraestructure.security;
 
-public class JwtAuthenticationConverter {
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthenticationConverter implements ServerAuthenticationConverter {
+
+
+    @Override
+    public Mono<Authentication> convert(ServerWebExchange exchange) {
+        String token = resolveToken(exchange);
+
+        if (token != null ){
+            return Mono.just(new UsernamePasswordAuthenticationToken(token, token));
+        } else {
+            return Mono.empty();
+        }
+    }
+
+
+    private String resolveToken(ServerWebExchange exchange) {
+        String bearerToken = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
 }
