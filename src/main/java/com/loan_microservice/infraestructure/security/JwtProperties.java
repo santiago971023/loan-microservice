940ea0ce347e6be8dc2c99a7ee67c301ccc4b2d1
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-25 12:01:37 -0500

    feat (creación de solicitudes): Se crea clase JwtProperties.java, representación de nuestro secret y nuestro expiration.

---
+++ src/main/java/com/loan_microservice/infraestructure/security/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.loan_microservice.infraestructure.security;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Data
+public class JwtProperties {
+
+    private String secret;
+    private Long expiration;
+
+}
