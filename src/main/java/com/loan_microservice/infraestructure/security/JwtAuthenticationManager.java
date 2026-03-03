df8c08b6682efa7af94657f57be490f7afb64426
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-03-03 13:51:19 -0500

    feat (creación de solicitudes): Se agregan líneas en el método authenticate para manejar posibles errores (como falta de algún claim)

--- src/main/java/com/loan_microservice/infraestructure/security/JwtAuthenticationManager.java
+++ src/main/java/com/loan_microservice/infraestructure/security/JwtAuthenticationManager.java
@@ -1,5 +1,7 @@
 package com.loan_microservice.infraestructure.security;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -14,6 +16,7 @@ import java.util.List;
 
 
 @Component
+@Slf4j
 public class JwtAuthenticationManager implements ReactiveAuthenticationManager {
 
     private final JwtValidator jwtValidator;
@@ -26,15 +29,28 @@ public class JwtAuthenticationManager implements ReactiveAuthenticationManager {
     public Mono<Authentication> authenticate(Authentication authentication) {
         return Mono.just(authentication.getCredentials().toString())
                 .map(token -> {
+                    // Extraigo Claims
                     Claims claims = jwtValidator.getClaimsFromToken(token);
+
+                    // Extraigo los datos
                     String email = claims.get("email", String.class);
                     Date expiry = claims.getExpiration();
                     String role = claims.get("role", String.class);
                     Long userId = claims.get("userId", Long.class);
+
+                    // Validación
+                    if(userId == null || role == null || email == null){
+                        log.warn("Token rechazado: Faltan Claims obligatorios.");
+                        throw new BadCredentialsException("Token Incompleto.");
+                    }
+
                     SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
                     CustomPrincipal principal = new CustomPrincipal(userId, email);
                     return (Authentication) new UsernamePasswordAuthenticationToken(principal, null, List.of(authority));
                 })
-                .onErrorResume(e -> Mono.empty());
+                .onErrorResume(e -> {
+                    log.error("Error validando JWT: {}", e.getMessage());
+                    return Mono.empty();
+                });
     }
 }
