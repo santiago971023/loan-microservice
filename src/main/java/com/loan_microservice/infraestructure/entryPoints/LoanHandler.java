737c95a6419d32e0b438410eef051a340306ec0c
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-03-03 14:23:19 -0500

    feat (creación de solicitudes): Se agrega la validación de suplantación, comparando el dni del request con el dni que viene del auth-microservice en el token.

--- src/main/java/com/loan_microservice/infraestructure/entryPoints/LoanHandler.java
+++ src/main/java/com/loan_microservice/infraestructure/entryPoints/LoanHandler.java
@@ -2,10 +2,13 @@ package com.loan_microservice.infraestructure.entryPoints;
 
 import com.loan_microservice.application.ports.in.CreateLoanInputPort;
 import com.loan_microservice.infraestructure.entryPoints.dto.LoanRequestDto;
+import com.loan_microservice.infraestructure.entryPoints.exception.ImpersonationNotAllowedException;
 import com.loan_microservice.infraestructure.mapper.LoanRestMapper;
+import com.loan_microservice.infraestructure.security.CustomPrincipal;
 import com.loan_microservice.infraestructure.shared.RequestValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -28,8 +31,29 @@ public class LoanHandler {
     }
 
     public Mono<ServerResponse> createLoan(ServerRequest serverRequest) {
-        return serverRequest.bodyToMono(LoanRequestDto.class)
-                .doOnNext(dto -> log.info("Solicitud de préstamo recibida para DNI: {}", dto.customerDni()))
+
+        // FLujo del body
+        Mono<LoanRequestDto> dtoMono = serverRequest.bodyToMono(LoanRequestDto.class)
+            .doOnNext(dto -> log.info("Solicitud para el DNI: {}", dto.customerDni()));
+
+        // Flujo de security
+        Mono<CustomPrincipal> principalMono = ReactiveSecurityContextHolder.getContext()
+                .map(ctx -> (CustomPrincipal) ctx.getAuthentication().getPrincipal());
+
+
+        return Mono.zip(dtoMono, principalMono)
+                .flatMap(tuple -> {
+                    LoanRequestDto dto = tuple.getT1();
+                    CustomPrincipal principal = tuple.getT2();
+
+                    // Validación de seguridad, comparamos el DNI
+                    if(!principal.dni().equals(dto.customerDni())){
+                        log.error("SUPLANTACIÓN: Token DNI {} intenta pedir un crédito para DNI {}", principal.dni(), dto.customerDni());
+                        return Mono.error(new ImpersonationNotAllowedException("No puedes solicitar un crédito para otra persona"));
+                    }
+                    return Mono.just(dto);
+                })
+
                 .flatMap(requestValidator::validate)
                 .map(loanRestMapper::toDomain)
                 .flatMap(createLoanInputPort::create)
