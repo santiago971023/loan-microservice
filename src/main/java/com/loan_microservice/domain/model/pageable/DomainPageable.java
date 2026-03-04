1092a06f92e00de6046d167798dc998e07f50935
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-03-04 12:06:20 -0500

    feat (creación de solicitudes): Se crea clase DomainPageable, será la representación en Dominio de la clase que nos brinda el adaptador en infraestructura.

---
+++ src/main/java/com/loan_microservice/domain/model/pageable/DomainPageable.java
@@ -0,0 +1,28 @@
+package com.loan_microservice.domain.model.pageable;
+
+public class DomainPageable {
+
+    private int pageNumber;
+    private int pageSize;
+
+    public DomainPageable(int pageNumber, int pageSize) {
+        this.pageNumber = pageNumber;
+        this.pageSize = pageSize;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+}
