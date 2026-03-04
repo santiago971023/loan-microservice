1c0a2fe3106ba200cc78db32e70b6c8a073a0e1a
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-03-04 13:50:13 -0500

    feat (HU04: Listar Solicitudes Pendientes): Se agrega método para buscar usuario por dni en el puerto UserServiceOutPort.java, método necesario en el caso de uso de listar las LoanApplication

--- src/main/java/com/loan_microservice/application/ports/out/UserServiceOutPort.java
+++ src/main/java/com/loan_microservice/application/ports/out/UserServiceOutPort.java
@@ -1,9 +1,11 @@
 package com.loan_microservice.application.ports.out;
 
+import com.loan_microservice.domain.model.user.User;
 import reactor.core.publisher.Mono;
 
 public interface UserServiceOutPort {
 
     Mono<Boolean> existsByDni(String dni);
+    Mono<User> findUserByDni(String dni);
 
 }
