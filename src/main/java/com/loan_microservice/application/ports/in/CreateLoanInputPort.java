46fa2bd547a7a4e826351ffb9691d863a23dbc75
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-03-04 12:05:41 -0500

    feat (creación de solicitudes): Se crea paquete de Loan para mejor organización

--- src/main/java/com/loan_microservice/application/ports/in/CreateLoanInputPort.java
+++ src/main/java/com/loan_microservice/application/ports/in/CreateLoanInputPort.java
@@ -1,6 +1,6 @@
 package com.loan_microservice.application.ports.in;
 
-import com.loan_microservice.domain.model.LoanApplication;
+import com.loan_microservice.domain.model.loan.LoanApplication;
 import reactor.core.publisher.Mono;
 
 public interface CreateLoanInputPort {
