f09c7f6f00c138332e06dd37f07953952021033d
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-03-04 13:49:14 -0500

    feat (HU04: Listar Solicitudes Pendientes): Se agrega método para buscar préstamos filtrando por estado en el LoanRepositoryOutPort.java.

--- src/main/java/com/loan_microservice/application/ports/out/LoanRepositoryOutPort.java
+++ src/main/java/com/loan_microservice/application/ports/out/LoanRepositoryOutPort.java
@@ -1,10 +1,15 @@
 package com.loan_microservice.application.ports.out;
 
-import com.loan_microservice.domain.model.LoanApplication;
+import com.loan_microservice.domain.model.loan.LoanApplication;
+import com.loan_microservice.domain.model.loan.LoanStatus;
+import com.loan_microservice.domain.model.pageable.DomainPageable;
+import com.loan_microservice.domain.model.pageable.PaginationResponse;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public interface LoanRepositoryOutPort {
 
     Mono<LoanApplication> save(LoanApplication loanApplication);
-
+    Mono<PaginationResponse<LoanApplication>> findLoanByStatus(List<LoanStatus> status, DomainPageable pageable);
 }
